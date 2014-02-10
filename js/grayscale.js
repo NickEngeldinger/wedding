e58f949b412ee791f347b86d2a193307c7f2721c
user <user@lubuntu.(none)>
2014-02-09 19:21:23 -0500

    Added button styling

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,22 +1,9 @@
-var navOffset = $('.navbar').offset().top
-
-console.log(navOffset)
-
-if (  navOffset < 50) {
-    $('navbar-fixed-top').addClass("top-nav-collapse").hide();
-}
-
 //jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
-    var nav = $('.navbar-fixed-top');
-
     if ($(".navbar").offset().top > 50) {
-        nav.addClass("top-nav-collapse");
-        nav.show();
-    } 
-    else {
-        nav.removeClass("top-nav-collapse");
-        nav.hide();
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
 
